02bca897bdfc1d73e935fd02e927ca330dd5ba1a
ddu <sujeongyee0529@gmail.com>
2023-09-24 18:23:32 +0900

    build

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.use(
     '/api', // 이 경로로 들어오는 요청을 프록시할 것입니다.
     createProxyMiddleware({
-      target: 'http://13.124.230.133:8888/', // 프록시할 대상 서버 주소를 여기에 지정합니다.
+      target: 'http://13.124.230.133:8888', // 프록시할 대상 서버 주소를 여기에 지정합니다.
       changeOrigin: true
     })
   );
